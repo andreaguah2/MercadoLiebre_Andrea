495facc8397b26eb2566bebb8a48e63c4afa95b1
andreaguah2 <andrea.aguah@gmail.com>
2022-07-18 21:41:25 -0500

    Ejercitacion 2

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ const app = express();
 // Estructuras de carpeta y Send File - Requiriendo Path
 const path = require('path');
 
-app.get('/', function(req, res) {
-    let htmlPath = path.resolve(__dirname,'./views/home.html');
-        res.sendFile(htmlPath);
-});
-
 //Servidor Web con Express - Levantando mi servidor
 app.listen(3002, ()=> 
     console.log('Servidor funcionando')  
 );
+
+//Atando carpetas de mi proyecto
+const publicPath = path.resolve(__dirname, 'public');
+    app.use(express.static(publicPath));
+
+
+app.get('/', function(req, res) {
+    let htmlPath = path.resolve(__dirname,'views/home.html');
+        res.sendFile(htmlPath);
+});
+
 
 //  Como ya tenemos la ruta absoluta con path, esta ya no es necesaria - app.get('/',function(req, res){res.send('Bienvenido a Mercado Liebre!')});
 
@@ -25,4 +31,3 @@ app.get('/contacto', function(req, res){
 app.get('/producto', function(req, res){
     res.send('Todo lo que necesitas!')
 });
-
