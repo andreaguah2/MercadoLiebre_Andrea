a9b502d2b7219076318905da1a703d65682f2a44
andreaguah2 <andrea.aguah@gmail.com>
2022-07-20 16:21:31 -0500

    Styles 1

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-// Introducción a Express - Llamando a express
 const express = require("express");
 const app = express();
-
-// Estructuras de carpeta y Send File - Requiriendo Path
 const path = require('path');
 
-//Servidor Web con Express - Levantando mi servidor
 app.listen(3002, ()=> 
-    console.log('Servidor funcionando')  
+    console.log('Al fin estoy funcionando')  
 );
 
-//Atando carpetas de mi proyecto
 const publicPath = path.resolve(__dirname, 'public');
     app.use(express.static(publicPath));
-
 
 app.get('/', function(req, res) {
     let htmlPath = path.resolve(__dirname,'views/home.html');
         res.sendFile(htmlPath);
 });
 
-
-//  Como ya tenemos la ruta absoluta con path, esta ya no es necesaria - app.get('/',function(req, res){res.send('Bienvenido a Mercado Liebre!')});
-
-//Introducción a Routing and response
 app.get('/contacto', function(req, res){
     res.send('Dejanos tu contacto!')
 });
